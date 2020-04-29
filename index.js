52126ced543c28a78a98bd29fd5bc5ab931d9ab8
Paweł Kalitowski <pawel.kalit@gmail.com>
2020-04-29 13:38:20 +0200

    Add info about version

--- index.js
+++ index.js
@@ -3,16 +3,28 @@ const github = require('@actions/github');
 const axios = require('axios').default;
 
 try {
-    const repoKey = core.getInput('repo-key');
     const branchRefName = core.getInput('branch');
-    const repoName = core.getInput('repo');
-
-    const payload = github.context.payload;
-    
-    const branchName = branchRefName.replace('refs/', '');
+    const repoName = core.getInput('repo');    
+    const branchName = branchRefName.replace('refs/', '').replace('heads/');
 
     console.log(`Current repo name: ${repoName}`);
     console.log(`Current branch name: ${branchName}`);
+
+    const versionRegex = new RegExp('^(?<mainBranch>.*)\\/(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<patch>\\d+)$');
+    const regexMatch = versionRegex.exec(branchName);
+
+    if(!regexMatch) {
+        console.log('Not need cascade merge');
+        return;
+    }
+
+    const major = regexMatch.groups.major;
+    const minor = regexMatch.groups.minor;
+    const patch = regexMatch.groups.patch;
+
+    console.log(`Major version: ${major}`);
+    console.log(`Major version: ${minor}`);
+    console.log(`Major version: ${patch}`);
 } catch (error) {
     core.setFailed(error.message);
 }
