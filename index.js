9e6c1aecdca0f877e8fb8c5908abe3633ecc64b6
Paweł Kalitowski <pawel.kalit@gmail.com>
2020-04-29 13:12:22 +0200

    Fix repo name

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const axios = require('axios').default;
 
 try {
     const repoKey = core.getInput('repo-key');
+    const branchRefName = core.getInput('branch');
+    const repoName = core.getInput('repo');
 
     const payload = github.context.payload;
     
-    const branchRefName = payload.ref;
     const branchName = branchRefName.replace('refs/', '');
 
-    console.log(`Current repo name: ${github.event.repository.name}`);
+    console.log(`Current repo name: ${repoName}`);
     console.log(`Current branch name: ${branchName}`);
 } catch (error) {
     core.setFailed(error.message);
