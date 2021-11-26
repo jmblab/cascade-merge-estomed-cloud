8ffd0788c7a05fbf805cf356c14f8d65496dbefc
Paweł Kalitowski <64414312+KalitLab@users.noreply.github.com>
2021-11-26 12:56:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const getReleaseData = require('./helpers').getReleaseData;
+
+function getReleaseData(branchName) {
+    const versionRegex = new RegExp('^(?<mainBranch>.*)\\/(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<patch>\\d+)$');
+    const regexMatch = versionRegex.exec(branchName);
+
+    if(!regexMatch) {
+        return null;
+    }
+
+    const major = regexMatch.groups.major * 1;
+    const minor = regexMatch.groups.minor * 1;
+    const patch = regexMatch.groups.patch * 1;
+
+    return { major, minor, patch }
+}
 
 async function run() {
     try {
